57a08b814251ef4d74cac5f73202374dce631959
Taylor carr <taylormichaelcarr@yahoo.com>
2024-03-01 09:21:26 +0000

    added generic post views

--- blog/models.py
+++ blog/models.py
@@ -5,16 +5,24 @@ STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
 class Post(models.Model):
+
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
     User, on_delete=models.CASCADE, related_name="blog_posts")
 
-content = models.TextField()
-created_on = models.DateTimeField(auto_now_add=True)
-status = models.IntegerField(choices=STATUS, default=0)
-excerpt = models.TextField(blank=True)
-updated_on = models.DateTimeField(auto_now=True)
+
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
 
 class Comment(models.Model):
     post = models.ForeignKey(
@@ -22,6 +30,12 @@ class Comment(models.Model):
     author = models.ForeignKey(
     User, on_delete=models.CASCADE, related_name="commenter")
     
-body = models.TextField()
-approved = models.BooleanField(default=False)
-created_on = models.DateTimeField(auto_now_add=True)
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
